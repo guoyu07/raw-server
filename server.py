8f31bb1d9b9349231ed0748f2a4970603bf3d5ae
kamrul hasan <mailid127@gmail.com>
2016-09-05 07:29:54 +0600

    implement create_page, send_page method

--- server.py
+++ server.py
@@ -4,21 +4,43 @@ import http.server
 class RequestHandler(http.server.BaseHTTPRequestHandler):
     '''Handle HTTP request by sending a fixed 'page'. '''
     # page to send back
-    page = b'''\
+    Page = '''\
         <html>
             <body>
-                <p>We are on web!</p>
+                <table>
+                    <tr>  <td>Header</td>         <td>Value</td>          </tr>
+                    <tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
+                    <tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
+                    <tr>  <td>Client port</td>    <td>{client_port}s</td> </tr>
+                    <tr>  <td>Command</td>        <td>{command}</td>      </tr>
+                    <tr>  <td>Path</td>           <td>{path}</td>         </tr>
+                </table>
             </body>
         </html>
     '''
 
     # Handle a GET request.
     def do_GET(self):
+        page = self.create_page()
+        self.send_page(page)
+
+    def create_page(self):
+        values = {
+        'date_time' : self.date_time_string(),
+        'client_host' : self.client_address[0],
+        'client_port' : self.client_address[1],
+        'command' : self.command,
+        'path' : self.path
+        }
+        page = self.Page.format(**values)
+        return page.encode('utf-8')
+
+    def send_page(self, page):
         self.send_response(200)
         self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Length', str(len(self.page)))
+        self.send_header('Content-Length', str(len(page)))
         self.end_headers()
-        self.wfile.write(self.page)
+        self.wfile.write(page)
 
 #-------------------------------------------------------------------------------
 
