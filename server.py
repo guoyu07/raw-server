54478010751084cf34b8891847e69bcdb211ff25
kamrul hasan <mailid127@gmail.com>
2016-09-06 07:33:38 +0600

    add index.html feature

--- server.py
+++ server.py
@@ -1,8 +1,59 @@
 import os
 import http.server
+from abc import ABCMeta, abstractmethod
+
+
+class Case(metaclass=ABCMeta):
+    @abstractmethod
+    def test(self, handler):
+        pass
+
+    @abstractmethod
+    def act(self, handler):
+        pass
+
+class case_directory_index_file(Case):
+    '''serve index.html for a directory!'''
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+    def test(self, handler):
+        return os.path.isdir(handler.full_path) and \
+               os.path.isfile(self.index_path(handler))
+    def act(self, handler):
+        handler.handle_file(self.index_path(handler))
+
+class case_no_file(Case):
+    ''' File or directory does not exists!'''
+    def test(self, handler):
+        return not os.path.exists(handler.full_path)
+
+    def act(self, handler):
+        raise ServerException("'{0}' not found!".format(handler.path))
+
+class case_existing_file(Case):
+    '''File exists!'''
+    def test(self, handler):
+        return os.path.isfile(handler.full_path)
+    def act(self, handler):
+        handler.handle_file(handler.full_path)
+
+class case_always_fail(Case):
+    '''Base case if nothing else worked!'''
+    def test(self, handler):
+        return True
+
+    def act(self, handler):
+        raise ServerException("Unknown object '{0}'".format(handler.path))
 
 
 class RequestHandler(http.server.BaseHTTPRequestHandler):
+
+    '''\
+    If requested file is found then its serverd!
+    If anything else is happened error page is constructed!
+    '''
+    Cases = [case_no_file, case_existing_file, \
+            case_directory_index_file, case_always_fail]
 
     Error_page = '''\
         <html>
@@ -19,16 +70,17 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         # page = self.create_page()
         # self.send_page(page)
         try:
-            # figure out what is being requested
-            full_path = os.getcwd()+self.path
+            # figure out what is being requested.
+            self.full_path = os.getcwd()+self.path
 
-            if not os.path.exists(full_path):
-                raise ServerException("'{0}' not found".format(self.path))
-            elif os.path.exists(full_path):
-                self.handle_file(full_path)
-            else:
-                raise ServerException("Unknown object '{0}' ".format(self.path))
-
+            # figure out how to handle.
+            for case in self.Cases:
+                # print(case.__name__)
+                handler = case()
+                if handler.test(self):
+                    handler.act(self)
+                    break
+        # Handle errors
         except Exception as msg:
             self.handle_error(msg)
 
